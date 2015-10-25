b5ecae8f97d25de0e9722ad81ab523573f938aba
Dan Davison <dandavison7@gmail.com>
2015-10-25 01:22:49 -0700

    Simplify initial graph

--- music_graph/write_graph.py
+++ music_graph/write_graph.py
@@ -7,24 +7,8 @@ from settings import GRAPH_FILE
 
 
 def make_graph(music_dir):
-    artists = os.listdir(music_dir)
-
-    group_fn = lambda artist: int(a[0].lower() < 'm')
-
-    groups = {
-        0: [],
-        1: [],
-    }
-    for a in artists:
-        groups[group_fn(a)].append(a)
-
     g = nx.Graph()
-    for a1, a2 in zip(groups[0], groups[1]):
-        g.add_edge(a1, a2)
-
-    for n in g:
-        g.node[n]['name'] = n
-
+    g.add_nodes_from(os.listdir(music_dir))
     return g
 
 
