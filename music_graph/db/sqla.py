1f23d16600ead230d0de3c4af6ddab29497d5d79
Dan Davison <dandavison7@gmail.com>
2015-11-14 00:30:50 -0800

    Convert add_google_ids_to_library to sqla

--- music_graph/db/sqla.py
+++ music_graph/db/sqla.py
@@ -7,5 +7,9 @@ ENGINE = sqla.create_engine(DATABASE_URI)
 METADATA = sqla.MetaData(bind=ENGINE)
 
 
+def fetchall(*args, **kwargs):
+    return ENGINE.connect().execute(*args, **kwargs).fetchall()
+
+
 def get_table(name):
     return sqla.Table(name, METADATA, autoload=True)
